2a7b2dce28df7a30403f724e6598b03370e64e36
george-dimitrov <george_hd@abv.bg>
2017-10-22 23:21:01 +0300

    two more exercises added

---
+++ Harvest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            byte workers = byte.Parse(Console.ReadLine());
+
+            int kg = (int) (x * y);
+            double wine = kg * 40 / 100 / 2.5;
+
+            if (wine >= z)
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine)} liters.");
+                Console.WriteLine($"{wine - z} liters left -> {Math.Ceiling((wine - z) / workers)} liters per person.");
+            }
+            else
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(z - wine)} liters wine needed.");
+            }
+        }
+    }
+}
